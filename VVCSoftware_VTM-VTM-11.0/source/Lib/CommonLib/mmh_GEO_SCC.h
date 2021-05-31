c39bc6cdf7e89b18e372284f5fdb4b003ac29eda
mominghong <1512415885@qq.com>
2021-05-31 09:48:19 +0800

    GEO_SCC_extractData

---
+++ VVCSoftware_VTM-VTM-11.0/source/Lib/CommonLib/mmh_GEO_SCC.h
@@ -0,0 +1,13 @@
+
+#ifndef __MMH_GEO_SCC_H__
+#define __MMH_GEO_SCC_H__
+
+#define JVET_P0069_reproduction     0   //∏¥œ÷JVET_P0069
+#define JVET_Q0060_reproduction     0   //∏¥œ÷JVET_Q0060
+
+
+
+#include <sys/timeb.h>
+#define extract_data                1
+
+#endif
